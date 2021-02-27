8e5fe5de872ed9d80e5e641f79af7098262c3b52
Daniel Kiedrowicz <33001747+xsudoo@users.noreply.github.com>
2021-02-27 17:54:51 +0100

    Add employee field

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,4 +1,47 @@
 package org.example;
 
 public class Employee {
+    private int id;
+    private String name;
+    private String lastName;
+
+
+    public Employee(int id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
